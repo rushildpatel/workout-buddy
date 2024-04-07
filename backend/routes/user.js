bed052b92c82722640e428e311f305f2d67e443f
Rushil Patel <rushildineshpatel2000@gmail.com>
2024-04-07 11:51:03 -0400

    added userController.js file to write functions for login and signup users

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
+// constroller functions
+const { signupUser, loginUser } = require("../controllers/userController");
+
 const Router = express.Router();
 
 // login route
-router.post("/login", () => {});
+router.post("/login", loginUser);
 
 // signup route
-router.post("/signup", () => {});
+router.post("/signup", signupUser);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
+
 const express = require("express");
 const workoutRoutes = require("./routes/workouts");
+const userRoutes = require("./routes/user");
 const mongoose = require("mongoose");
 
 //express app
@@ -16,6 +18,7 @@ app.use((req, res, next) => {
 
 //routes
 app.use("/api/workouts", workoutRoutes);
+app.use("/api/user", userRoutes);
 
 //connect to db
 mongoose
