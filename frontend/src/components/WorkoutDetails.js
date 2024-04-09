ba465d1935bb317c325c4ab3d804865afddb60d3
Rushil Patel <rushildineshpatel2000@gmail.com>
2024-04-09 12:55:13 -0400

    making authorized request from frontend

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,14 +1,23 @@
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 // date fns
 import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
 const WorkoutDetails = ({ workout }) => {
   const { dispatch } = useWorkoutsContext();
+  const { user } = useAuthContext();
 
   const handleClick = async () => {
+    if (!user) {
+      return;
+    }
+
     const response = await fetch("/api/workouts/" + workout._id, {
       method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
     });
     const json = await response.json();
 
@@ -25,7 +34,7 @@ const WorkoutDetails = ({ workout }) => {
         {workout.load}
       </p>
       <p>
-        <strong>Number of reps: </strong>
+        <strong>Reps: </strong>
         {workout.reps}
       </p>
       <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
